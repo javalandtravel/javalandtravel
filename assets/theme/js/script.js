18310f9b04a89cca720a314046d90dfdd7e815db
javalandtravel <javalandtravel@gmail.com>
2017-08-19 17:53:16 +0700

    Signed-off-by: javalandtravel <javalandtravel@gmail.com>

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -553,9 +553,9 @@
         $(document).ready(function() {
             // disable animation on scroll on mobiles
             if ($.isMobile()) {
-            //    return;
+              //  return;
                 // enable animation on scroll
-            }  if ($('input[name=animation]').length) {
+            } if ($('input[name=animation]').length) {
                 $('input[name=animation]').remove();
 
                 var $animatedElements = $('p, h1, h2, h3, h4, h5, a, button, small, img, li, blockquote, .mbr-author-name, em, label, input, textarea, .input-group, .iconbox, .btn-social, .mbr-figure, .mbr-map, .mbr-testimonial .card-block, .mbr-price-value, .mbr-price-figure, .dataTable, .dataTables_info').not(function() {
@@ -592,7 +592,7 @@
                                 .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
                                     $element.removeClass('animated fadeInUp');
                                 });
-                        }
+                        } 
                     });
                 }
 
@@ -600,7 +600,16 @@
                 $window.on('scroll resize', checkIfInView);
                 $window.trigger('scroll');
             }
-        });
+        
+	$animatedElements.bind('inview', function(event, visible) {
+      if (visible) {
+      //  
+      } else {
+        $(this).addClass('hidden animated');
+      }
+    });
+		
+		});
 
         if ($('.nav-dropdown').length) {
             $(".nav-dropdown").swipe({
@@ -1237,3 +1246,10 @@
     } 
 })(jQuery);
 !function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise</a> Mobirise v4.1.4';document.body.insertBefore(a,document.body.childNodes[0])}}();
+
+
+
+
+(function(d){var p={},e,a,h=document,i=window,f=h.documentElement,j=d.expando;d.event.special.inview={add:function(a){p[a.guid+"-"+this[j]]={data:a,$element:d(this)}},remove:function(a){try{delete p[a.guid+"-"+this[j]]}catch(d){}}};d(i).bind("scroll resize",function(){e=a=null});!f.addEventListener&&f.attachEvent&&f.attachEvent("onfocusin",function(){a=null});setInterval(function(){var k=d(),j,n=0;d.each(p,function(a,b){var c=b.data.selector,d=b.$element;k=k.add(c?d.find(c):d)});if(j=k.length){var b;
+if(!(b=e)){var g={height:i.innerHeight,width:i.innerWidth};if(!g.height&&((b=h.compatMode)||!d.support.boxModel))b="CSS1Compat"===b?f:h.body,g={height:b.clientHeight,width:b.clientWidth};b=g}e=b;for(a=a||{top:i.pageYOffset||f.scrollTop||h.body.scrollTop,left:i.pageXOffset||f.scrollLeft||h.body.scrollLeft};n<j;n++)if(d.contains(f,k[n])){b=d(k[n]);var l=b.height(),m=b.width(),c=b.offset(),g=b.data("inview");if(!a||!e)break;c.top+l>a.top&&c.top<a.top+e.height&&c.left+m>a.left&&c.left<a.left+e.width?
+(m=a.left>c.left?"right":a.left+e.width<c.left+m?"left":"both",l=a.top>c.top?"bottom":a.top+e.height<c.top+l?"top":"both",c=m+"-"+l,(!g||g!==c)&&b.data("inview",c).trigger("inview",[!0,m,l])):g&&b.data("inview",!1).trigger("inview",[!1])}}},250)})(jQuery);
